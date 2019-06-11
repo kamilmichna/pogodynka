4a202c99b97ae37733c9fcde282e0db867dd7bce
kamilmichna <kamil.michna28@gmail.com>
2019-06-11 23:24:06 +0200

    Updates

---
+++ precache-manifest.bc505450f0ea04849adf6b16638bfce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363fe496e0945ea860d835393146e05d",
+    "url": "/pogodynka/index.html"
+  },
+  {
+    "revision": "0a650619f4efe12062e7",
+    "url": "/pogodynka/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "1dd789b1ad550d99c837",
+    "url": "/pogodynka/static/js/2.59ae5c1c.chunk.js"
+  },
+  {
+    "revision": "0a650619f4efe12062e7",
+    "url": "/pogodynka/static/js/main.dade156a.chunk.js"
+  },
+  {
+    "revision": "210bcda73b87a7d902f3",
+    "url": "/pogodynka/static/js/runtime~main.ba660732.js"
+  },
+  {
+    "revision": "26c57035bc630c3b1d0bb30ffdfd7c1c",
+    "url": "/pogodynka/static/media/landing_img.26c57035.svg"
+  }
+]);
